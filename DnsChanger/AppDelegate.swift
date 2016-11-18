664dd24acdecf3889479a8b7cb6f6d56be39070a
Peter Young <young40@gmail.com>
2016-11-18 12:56:39 +0800

    Initial Commit

---
+++ DnsChanger/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DnsChanger
+//
+//  Created by Peter Young on 18/11/2016.
+//  Copyright © 2016 Peter Young. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
