8c07c3a508ad3218dfbf74a39d16f041570fdc96
Peter Young <young40@gmail.com>
2016-11-24 19:20:36 +0800

    update dns by menu

--- DnsChanger/AppDelegate.swift
+++ DnsChanger/AppDelegate.swift
@@ -19,23 +19,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let data = self.getData()
         
-        self.initMenu(data: data)
+        let dns = self.getDns()
+        
+        self.initMenu(data: data, dnsList: dns)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-    func initMenu(data: [String: [String]]) {
+    func initMenu(data: [String: [String]], dnsList: [String]) {
         let menu = NSMenu(title: "DnsChanger")
         
         let itemDefault = NSMenuItem(title: "Default", action: #selector(setDefaultDns), keyEquivalent: "")
         menu.addItem(itemDefault)
+        if dnsList.count == 0 {
+            itemDefault.state = NSOnState
+        }
+        
         menu.addItem(NSMenuItem.separator())
         
         if let dns = data["dns"] {
-            for d  in dns {
+            for d in dns {
                 let itemNew = NSMenuItem(title: d, action: #selector(setDnsByMenu), keyEquivalent: "")
+                
+                if dnsList.contains(d) {
+                    itemNew.state = NSOnState
+                }
                 
                 menu.addItem(itemNew)
             }
@@ -66,13 +76,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return ["dns": ["119.29.29.29"]]
     }
     
-    func setDefaultDns(sneder: AnyObject) {
+    func setDefaultDns(sender: NSMenuItem) {
         self.setDns(dns: "empty")
+        
+        let dns = self.getDns()
+        statusItem.menu?.item(at: 0)?.state = dns.count > 0 ? NSOffState : NSOnState
+        
+        for index in  1 ... statusItem.menu!.numberOfItems - 1 {
+            if let item = statusItem.menu!.item(at: index) {
+                if !item.isSeparatorItem {
+                    item.state = dns.contains(item.title) ? NSOnState : NSOffState
+                }
+            }
+        }
     }
     
     func setDnsByMenu(sender: NSMenuItem) {
-        let dns = sender.title
-        self.setDns(dns: dns)
+        let title = sender.title
+        
+        var dnsBefor = self.getDns()
+        
+        if dnsBefor.contains(title) {
+            dnsBefor = dnsBefor.filter() {$0 != title}
+        } else {
+            dnsBefor.append(title)
+        }
+        var newDNS = dnsBefor.joined(separator: " ")
+        
+        if dnsBefor.count == 0 {
+            newDNS = "empty"
+        }
+        
+        self.setDns(dns: newDNS)
+        
+        let dns = self.getDns()
+        statusItem.menu?.item(at: 0)?.state = dns.count > 0 ? NSOffState : NSOnState
+        sender.state = dns.contains(title) ? NSOnState : NSOffState
     }
     
     func exit(sender: AnyObject) {
@@ -84,6 +123,30 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let appleScript = NSAppleScript.init(source: shell)
         appleScript?.executeAndReturnError(nil)
+    }
+    
+    private func getDns() -> [String] {
+        var dns: [String] = []
+        
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.launchPath = "/usr/sbin/networksetup"
+        task.arguments = ["-getdnsservers", "Wi-Fi"]
+        task.standardOutput = pipe
+        
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        if let rs = String(data: data, encoding: String.Encoding.utf8) {
+            let c = rs[rs.startIndex ... rs.index(rs.startIndex, offsetBy: 0)]
+            
+            if let num = Int(c) {
+                dns = rs.characters.split(separator: "\n").map(String.init)
+            }
+        }
+        
+        return dns
     }
 }
 
